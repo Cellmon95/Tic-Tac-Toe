a045fcd8370040d3b56283865ba9c68b507f443b
Cellmon95 <lucas.arne@hotmail.com>
2023-05-24 18:40:10 +0200

    worked on the layout and cells

--- tic-tac-toe/src/routes/+page.svelte
+++ tic-tac-toe/src/routes/+page.svelte
@@ -1,5 +1,17 @@
-<h1>{msg}</h1>
-
-<script lang="ts">
-    let msg:string = 'Hello World';
+<script>
+import Cell from "./Cell.svelte";
 </script>
+
+<main>
+    <div class="grid">
+        <Cell/>
+        <Cell/>
+        <Cell/>
+        <Cell/>
+        <Cell/>
+        <Cell/>
+        <Cell/>
+        <Cell/>
+        <Cell/>
+    </div>
+</main>
---
+++ tic-tac-toe/src/routes/Cell.svelte
@@ -0,0 +1,42 @@
+<style>
+    .cell{
+        border: solid;
+    }
+
+    .cell img{
+        width: 100%;
+        height: 100%;
+    }    
+</style>
+
+<script lang="ts">
+    import { empty } from "svelte/internal";
+import cross from "../assets/img/cross.svg";
+
+    enum States{
+        Cross,
+        Circle,
+        Empty
+    }
+
+    let src:string = "";
+    let currentState:States = States.Empty;
+    
+
+    function onClick(event:Event):void {
+        if (currentState === States.Empty) {
+            src = cross;
+            currentState = States.Cross;
+        }
+    }
+
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="cell" on:click={onClick}>
+    {#if currentState !== States.Empty}
+        <img {src} alt="cell">
+    {/if}
+
+</div>
+
